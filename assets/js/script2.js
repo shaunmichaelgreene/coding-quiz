ffbc87357d05e6a6b24f5b048cdebc2171e45337
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-04-25 18:19:56 -0400

    fixed individual score saving

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,3 +1,4 @@
+debugger;
 var scoreList = document.querySelector("#scores");
 var user = localStorage.getItem('user');
 var savedUser = JSON.parse(user);
@@ -8,10 +9,16 @@ var score1 = document.createElement('li');
 score1.textContent = ("Initials: " + savedUser.initials + " | Score: " + savedUser.score);
 scoreList.appendChild(score1);
 
-var clearScores = document.querySelector("#clear-scores")
-clearScores.addEventListener("click", localStorage.clear())
-clearScores.addEventListener("click", score1.remove())
-var goBack = document.querySelector("#go-back")
-goBack.onclick = function() {
+//temporarily removing the below logic while debugging
+document.querySelector("#clear-scores").onclick = function () {
+    localStorage.clear();
+    console.log("the clear-scores button element has registered a click!");
+};
+// var clearScores = document.querySelector("#clear-scores")
+// clearScores.addEventListener("click", localStorage.clear())
+// clearScores.addEventListener("click", score1.remove())
+// var goBack = document.querySelector("#go-back")
+document.querySelector("#go-back").onclick = function() { //redirects to home page 
     window.location.replace("https://shaunmichaelgreene.github.io/coding-quiz/index.html");
+    console.log("the go-back button element has registered a click!");
 };
