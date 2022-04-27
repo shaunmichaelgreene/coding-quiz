310a55abda8a1e8674bb124228b987f043a3346b
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-04-26 21:19:11 -0400

    localStorage issue resolved, array parsing added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# coding-quiz
+# Shaun's Coding Quiz
+
+Welcome to the README for Shaun's Coding Quiz!
+
+**User Story**: AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+This app utilizes HTML, CSS, Javascript, & Web APis to create a multiple-choice quiz with the ability to save the user scores. The user has 60 seconds to answer 5 questions; earning 1 point for each correct answer, and receiving a -10 second penalty for each incorrect answer. Upon completion, the user has the option to save their score, and can view and clear the bank of saved scores. 
+
+**Live Deployment Link**: https://shaunmichaelgreene.github.io/coding-quiz/index.html
+
+**Screenshot**: [![screenshot.jpg](https://i.postimg.cc/vmcrs2kD/screenshot.jpg)](https://postimg.cc/jWYw6csr)
+
+
+**Special Thanks To**: Brennan Spicer, Colton Watkins, Cabral Williams, & Alana McKeel
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,27 +1,16 @@
 var scoreList = document.querySelector("#scores");
-var user = localStorage.getItem('user');
+var user = localStorage.getItem("user");
 var savedUser = JSON.parse(user);
 console.log(savedUser.initials);
 console.log(savedUser.score);
 
-var scoreListItem = document.createElement('li');
-scoreListItem.textContent = ("Initials: " + savedUser.initials + " | Score: " + savedUser.score);
+var scoreListItem = document.createElement("li");
+scoreListItem.textContent =
+    "Initials: " + savedUser.initials + " | Score: " + savedUser.score;
 scoreList.appendChild(scoreListItem);
 
-//temporarily removing the below logic while debugging
 document.querySelector("#clear-scores").onclick = function () {
     localStorage.clear();
     console.log("the clear-scores button element has registered a click!");
-};
-var clearScores = document.querySelector("#clear-scores")
-clearScores.addEventListener("click", function () {
-    localStorage.clear()
-    // scoreListItem.remove()
-    scoreListItem.textContent = (' ');
-})
-
-var goBack = document.querySelector("#go-back")
-document.querySelector("#go-back").onclick = function() { //redirects to home page 
-    // window.location.replace("https://shaunmichaelgreene.github.io/coding-quiz/index.html");
-    console.log("the go-back button element has registered a click!");
+    scoreListItem.textContent = " ";
 };
